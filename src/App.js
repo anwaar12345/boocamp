9aeb2d73d374cf2f0040248d0f88abc900c01e8a
anwarshah <syed.anwar@nextgeni.dev>
2020-06-02 23:42:02 +0500

    react js surge cicd deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <?php echo ""Assalam O Alaikum"; ?>
+         echo "Assalam O Alaikum"; 
         </p>
         <a
           className="App-link"
