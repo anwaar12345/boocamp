f949225bef9199710c2baac1be8ec90c77fbd144
anwarshah <syed.anwar@nextgeni.dev>
2020-06-02 23:47:03 +0500

    react js surge cicd deployment

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Assalam O Alaikum 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="#"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
