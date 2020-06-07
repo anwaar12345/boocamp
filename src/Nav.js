fdfb6682835832fb698357f959d40a979ab2900a
anwarshah <syed.anwar@nextgeni.dev>
2020-06-08 02:39:47 +0500

    added

--- src/Nav.js
+++ src/Nav.js
@@ -8,7 +8,7 @@ function Nav({logo}) {
 };
    return (
 <nav className="navbar navbar-expand-lg navbar-light" style={style}>
-   <p className="navbar-brand">{logo}</p>
+   <a className="navbar-brand" href="#welcome">{logo}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -16,22 +16,20 @@ function Nav({logo}) {
     
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+        <a className="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="/qualification">Qualification</a>
+        <a className="nav-link" href="#qualification">Qualification</a>
       </li>
       <li className="nav-item active">
-        <a className="nav-link" href="/Portfolio">Portfolio</a>
+        <a className="nav-link" href="#Portfolio">Portfolio</a>
       </li> 
       <li className="nav-item active">
-        <a className="nav-link" href="/about">About Us</a>
+        <a className="nav-link" href="#about">About Us</a>
       </li>
-            <li className="nav-item active">
-        <a className="nav-link" href="/about">About Us</a>
-      </li>
+      
       <li className="nav-item active">
-        <a className="nav-link" href="/contact">Contact Us</a>
+        <a className="nav-link" href="#contact">Contact Us</a>
       </li>
     </ul>
   </div>
