09aea342449b2f80957754632aba6348681261fd
AnwaarAhmedshah <fa15be0011@maju.edu.pk>
2020-06-10 01:50:12 +0500

    class 4 final

--- src/Room.js
+++ src/Room.js
@@ -13,10 +13,15 @@ function Room() {
 const incTemp = () => {
     setTemp(++temp);
 }
-
+const style = {
+ marginTop: "10px",
+ fontSize: "30px",
+ marginBottom: "10px",
+ padding: "10px"
+};
 return (
   
-<div className={`App  ${isLit?"light":"dark"}`}>
+<div className={`App  ${isLit?"light":"dark"}`} style={style}>
 The Room is | {isLit ? "lit" : "Dark"}    
 <br/>
 <h4>Room Temperature : {temp} </h4>
